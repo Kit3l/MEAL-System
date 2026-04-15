f01335c2330f24ca1d145176ea734a3abfff95e2
Kit3l <pchumbakitel@gmail.com>
2026-04-15 22:27:44 +0300

    Final fixes

--- api.js
+++ api.js
@@ -603,10 +603,11 @@ cron.schedule("0 8 * * 1", async () => {
 // ─────────────────────────────────────────────────────────────────────────────
 
 const path = require("path");
-app.use(express.static(path.join(__dirname)));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/health", (req, res) => {
